06f0bb82a1578e98aa6cdc4d6b705dc2c032cbe8
= <pawaryash00@gmail.com>
2022-12-22 01:53:33 +0530

    assessment

--- Java-Script/Assessment-10/prototype.js
+++ Java-Script/Assessment-10/prototype.js
@@ -19,21 +19,61 @@ console.log(child.fullname())
 
 
 
-Array1 = [4,5,5,5,5];
+const parent2 = {
+    firstname : 'Yash',
+    lastname : 'Pawar',
+    City : 'Thane',
+    State : 'Maharashtra',
 
-
-const arraysum = {
-    addarray : function(array){
-        let sum =0;
-        for(let i = 0;i<array.lenght;i++){
-            sum =sum + array[i];
-
-        }
-        console.log(sum)
+    
+    Deatils(){
+        return (`${this.firstname} ${this.lastname} from ${this.City} in ${this.State} `)
     }
+
 }
 
-Array.__proto__=arraysum;
-Array.addarray(array1);
+const child2 = {
+    __proto__:parent2
+}
+const son1 = {
+    __proto__:child2
+}
+
+console.log(son1.Deatils());
+
+
+// Quetion-3
+            arr = [1, 2, 3, 4]
+            arr1 = [3, 2, 5, 4]
+            arr2 = [4, 2, 2, 4]
+                    
+            const array = {
+                    add: function (a){
+                        sum=0;
+                        for(i=0; i < a.length; i++){
+                            sum = sum + a[i]
+                        }
+                        console.log(sum);
+                    }
+                    }
+                    
+            Array.__proto__=array
+            Array.add(arr1)
+           
+    //   Quetion-4
+
+
+        var obj = {
+            name : "Yash ",
+            company : "TCS",
+            Stream : "Mechanical",
+            
+        }
+        result = Object.keys(obj)
+        console.log(result);
+          
+
+
+
 
 
--- Java-Script/Assessment-11/Assess11.js
+++ Java-Script/Assessment-11/Assess11.js
@@ -1,4 +1,20 @@
 
+// Quetion-1
+
+function callback (X,Y,add){
+    console.log(X);
+    add(5,6)
+    console.log(Y)
+
+}
+
+function add(a,b){
+    console.log(`sum of ${a + b}`)
+}
+
+callback(5,6,add)
+
+// Quetion-2
 
   const calculater = (num1, num2, callback) => {
         const result = callback(num1, num2)
@@ -24,4 +40,60 @@ setTimeout(() => {
         },1000)
     },1000)
 },1000)
+
+// Quetion-3
+
+let prntnumber = (tme, num) => {
+    return new Promise((res, rej) => {
+        if(num){
+            setTimeout(()=>{
+                res(num());
+            }, tme)
+        }
+        else{
+            rej(console.log("No Number"))
+        }
+
+    })
+}
+
+prntnumber(0, ()=>console.log("Number"))
+.then(()=>{
+    prntnumber(1000, ()=>console.log("1"))
+})
+.then(()=>{
+    prntnumber(2000, ()=>console.log("2"))
+})
+.then(()=>{
+    prntnumber(3000, ()=>console.log("3"))
+})
+.then(()=>{
+    prntnumber(4000, ()=>console.log("4"))
+})
+.then(()=>{
+    prntnumber(5000, ()=>console.log("5"))
+})
+.then(()=>{
+    prntnumber(6000, ()=>console.log("6"))
+})
+.then(()=>{
+    prntnumber(7000, ()=>console.log("7"))
+})
+
+// Quetion-4
+
+let x = function(condition){
+    return new Promise((resolve, reject)=>{
+        if(condition == 'yes'){
+            resolve('promise resolved')
+        }
+        else{
+            reject('promise rejected')
+        }
+    })
+}
+
+x('yes').then(response => console.log(response)).catch(err=>console.log(err))
+
+
    
