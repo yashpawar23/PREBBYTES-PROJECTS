e8a44a3ba2e0b059513dd5a1986a441cc341f02a
= <pawaryash00@gmail.com>
2022-12-22 19:27:32 +0530

    assess13

---
+++ Java-Script/Assessment-13/assess13.js
@@ -0,0 +1,8 @@
+function hideText(){
+    document.getElementById("show_hidden_text").style.display = 'none';
+}
+
+function showPara(){
+    document.getElementById("show_hidden_text").style.display = 'block';
+
+}
